3c848ec515c505c01ee21a506366fe9044d033a6
DESKTOP-OK2D2LF\donge <dongeal@gmail.com>
2023-06-11 18:26:24 +0900

    완료 그러나 게임 오버 없음

--- tetris.py
+++ tetris.py
@@ -1,14 +1,43 @@
 from settings import *
 import math
 from tetromino import Tetromino
+import pygame.freetype as ft
 
+class Text:
+    def __init__(self, app):
+        self.app = app
+        self.font = ft.Font(FONT_PATH)
 
+    def draw(self):
+        self.font.render_to(self.app.screen,(WIN_W * 0.595, WIN_H * 0.02),
+                            text= 'TETRIS', fgcolor= 'white',
+                            size=TILE_SIZE* 1.65, bgcolor='black')
+        self.font.render_to(self.app.screen,(WIN_W * 0.65, WIN_H * 0.22),
+                            text= 'next', fgcolor= 'orange',
+                            size=TILE_SIZE* 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen,(WIN_W * 0.64, WIN_H * 0.67),
+                            text= 'score', fgcolor= 'orange',
+                            size=TILE_SIZE* 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen,(WIN_W * 0.64, WIN_H * 0.8),
+                            text=f'{self.app.tetris.score}', fgcolor= 'white',
+                            size=TILE_SIZE* 1.8)
+       
 class Tetris:
     def __init__(self, app):
         self.app = app
         self.sprite_group = pg.sprite.Group()
         self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
+        self.next_tetromino = Tetromino(self, current=False)
+        self.speed_up = False
+
+        self.score = 0
+        self.full_lines = 0
+        self.points_per_line ={0:0, 1:100, 2:300, 3:700, 4:1500}
+    
+    def get_score(self):
+        self.score += self.points_per_line[self.full_lines]
+        self.full_lines = 0
 
     def check_full_lines(self):
         row = FIELD_H - 1
@@ -26,6 +55,8 @@ class Tetris:
                     self.field_array[row][x].alive = False
                     self.field_array[row][x] = 0
 
+                self.full_lines += 1
+
     def put_tetromino_blocks_in_array(self):
         for block in self.tetromino.blocks:
             x, y = int(block.pos.x), int(block.pos.y)
@@ -33,12 +64,24 @@ class Tetris:
 
     def get_field_array(self):
         return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
+    
+    def is_game_over(self):
+        if self.tetromino.blocks[0].pos.y == INIT_POS_OFFSET[1]:
+            pg.time.wait(300)
+            return True
 
     def check_tetromino_landing(self):
         if self.tetromino.landing:
-            self.put_tetromino_blocks_in_array()
-            self.tetromino = Tetromino(self)
+            if self.is_game_over():
+                # self.__init__(self.app)
+                pass
 
+            else:
+                self.speed_up = False
+                self.put_tetromino_blocks_in_array()
+                self.next_tetromino.current = True
+                self.tetromino = self.next_tetromino
+                self.next_tetromino = Tetromino(self, current=False)
 
     def control(self, pressed_key):
         if pressed_key == pg.K_LEFT:
@@ -47,6 +90,8 @@ class Tetris:
             self.tetromino.move(direction='right')
         elif pressed_key == pg.K_UP:
             self.tetromino.rotate()
+        elif pressed_key == pg.K_DOWN:
+            self.speed_up = True
 
     def draw_grid(self):
         for x in range(FIELD_W):
@@ -56,10 +101,14 @@ class Tetris:
                 
 
     def update(self):
-        if self.app.anim_trigger:  
+        trigger = [self.app.anim_trigger, self.app.fast_anim_trigger][self.speed_up]
+        if trigger:  
             self.check_full_lines()     
             self.tetromino.update()
             self.check_tetromino_landing()
+            self.get_score()
+            # self.speed_up = False  # 쭉 내려오는거 끔
+    
         self.sprite_group.update()
         
     def draw(self):
--- tetromino.py
+++ tetromino.py
@@ -5,16 +5,41 @@ class Block(pg.sprite.Sprite):
     def __init__(self, tetromino,pos):
         self.tetromino = tetromino
         self.pos = vec(pos) + INIT_POS_OFFSET
+        self.next_pos = vec(pos) + NEXT_POS_OFFSET
         self.alive = True
         
         super().__init__(tetromino.tetris.sprite_group)
-        self.image = pg.Surface([TILE_SIZE,TILE_SIZE])
-        pg.draw.rect(self.image, 'orange', (1, 1, TILE_SIZE-2, TILE_SIZE -2),border_radius=8)
+        self.image = tetromino.image
+        # self.image = pg.Surface([TILE_SIZE,TILE_SIZE])
+        # pg.draw.rect(self.image, 'orange', (1, 1, TILE_SIZE-2, TILE_SIZE -2),border_radius=8)
         self.rect = self.image.get_rect()
+
+        self.sfx_image = self.image.copy()
+        self.sfx_image.set_alpha(110)
+        self.sfx_speed = random.uniform(0.2, 0.6)
+        self.sfx_cycles = random.randrange(6, 8)
+        self.cycle_counter = 0
+
+    def sfx_end_time(self):
+        if self.tetromino.tetris.app.anim_trigger:
+            self.cycle_counter += 1
+            if self.cycle_counter > self.sfx_cycles:
+                self.cycle_counter = 0
+                return True
+
+
+    def sfx_run(self):
+        self.image = self.sfx_image
+        self.pos.y -= self.sfx_speed
+        self.image = pg.transform.rotate(self.image, 
+                        pg.time.get_ticks() * self.sfx_speed)
 
     def is_alive(self):
         if not self.alive:
-            self.kill()   
+            if not self.sfx_end_time():
+                self.sfx_run()
+            else:
+                self.kill()   
     
     def rotate(self, pivot_pos):
         translated = self.pos - pivot_pos  # A' = A - P
@@ -22,7 +47,8 @@ class Block(pg.sprite.Sprite):
         return rotated + pivot_pos         # A" + P 
 
     def set_rect_pos(self):
-        self.rect.topleft = self.pos * TILE_SIZE
+        pos= [self.next_pos, self.pos][self.tetromino.current]
+        self.rect.topleft = pos * TILE_SIZE
 
     def update(self):
         self.is_alive()
@@ -36,11 +62,31 @@ class Block(pg.sprite.Sprite):
         return True
 
 class Tetromino:
-    def __init__(self,tetris):
+    def __init__(self,tetris, current=True):
         self.tetris = tetris
         self.shape = random.choice(list(TETROMINOES.keys()))
+        
+        # 테트로미노 마다 색을 정할 경우.
+        if self.shape == 'T': 
+            self.image = tetris.app.images[0]
+        elif self.shape == 'O':
+            self.image = tetris.app.images[1] 
+        elif self.shape == 'J':
+            self.image = tetris.app.images[2] 
+        elif self.shape == 'L':
+            self.image = tetris.app.images[3] 
+        elif self.shape == 'I':
+            self.image = tetris.app.images[4]
+        elif self.shape == 'S':
+            self.image = tetris.app.images[5]
+        elif self.shape == 'Z':
+            self.image = tetris.app.images[6]
+        # 랜덤 타일색
+        # self.image = random.choice(tetris.app.images)
+
         self.blocks = [Block(self,pos) for pos in TETROMINOES[self.shape]]
         self.landing = False
+        self.current= current
 
     def rotate(self):
         pivot_pos = self.blocks[0].pos
@@ -58,7 +104,7 @@ class Tetromino:
         new_block_positions = [block.pos + move_direction for block in self.blocks]
         is_collide = self.is_collide(new_block_positions)
 
-        if not is_collide:
+        if not is_collide: # 모든 블록의 새위치에서 블록 충돌이 아니면 새위치 할당
             for block in self.blocks:
                 block.pos += move_direction
         elif direction == 'down':
